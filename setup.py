7c8552780ed74dfee97810ee0767cc7daa7e6150
zeio <zeionara@gmail.com>
2022-12-07 21:15:32 +0300

    Updated classifier in setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     ],
     install_requires=[],
     classifiers=[
-        'Programming Language :: Python :: 3.10.8',
+        'Programming Language :: Python :: 3.10',
     ],
     long_description = long_description,
     long_description_content_type = 'text/markdown'
